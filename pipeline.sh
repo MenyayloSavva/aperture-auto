7d9cd3577ebeac9d32af2743eb3167535698d752
Меняйло Савва Станиславович <smenyaylo@alfabank.ru>
2023-01-16 01:48:43 +0300

    fix pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -3,32 +3,31 @@
 # set variables
 APP_NAME=aperture-auto
 VERSION=0.0.1
-IMAGE_NAME=$APP_NAME:$VERSION
 DOCKER_HUB_USERNAME=savvamenyaylo
 DOCKER_HUB_PASSWORD=$(cat ~/.docker/password.txt)
-DOCKER_HUB_REPO_NAME=aperture
-DOCKER_HUB_IMAGE=$DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME:latest
+DOCKER_HUB_IMAGE=$DOCKER_HUB_USERNAME/$APP_NAME:latest
 USERNAME=root
 SERVER_IP=176.124.206.136
 
 echo "1. Build application $APP_NAME and create .jar file"
 ./gradlew clean build
 
-echo "2. Build docker image $IMAGE_NAME"
-docker rmi -f $IMAGE_NAME $DOCKER_HUB_IMAGE
-docker build -t $IMAGE_NAME -t $DOCKER_HUB_IMAGE .
+echo "2. Build docker image $DOCKER_HUB_IMAGE"
+docker rmi -f $DOCKER_HUB_IMAGE
+docker build -t $DOCKER_HUB_IMAGE .
 
 echo "3. Push $DOCKER_HUB_IMAGE to Docker Hub"
 echo $DOCKER_HUB_PASSWORD | docker login --username $DOCKER_HUB_USERNAME --password-stdin
 docker push $DOCKER_HUB_IMAGE
-docker rmi -f $IMAGE_NAME $DOCKER_HUB_IMAGE
+docker rmi -f $DOCKER_HUB_IMAGE
 
 echo "4. Ssh to $SERVER and run docker container"
 ssh $USERNAME@$SERVER_IP <<EOF
-    docker login --username $DOCKER_HUB_USERNAME --password "$DOCKER_HUB_PASSWORD"
-    docker rmi -f $DOCKER_HUB_IMAGE
-    docker pull $DOCKER_HUB_IMAGE
     docker stop $APP_NAME || true
     docker rm $APP_NAME || true
+    docker rmi -f $DOCKER_HUB_IMAGE
+
+    docker login --username $DOCKER_HUB_USERNAME --password "$DOCKER_HUB_PASSWORD"
+    docker pull $DOCKER_HUB_IMAGE
     docker run --network host --name $APP_NAME -d $DOCKER_HUB_IMAGE -p 8080:8080
 EOF
